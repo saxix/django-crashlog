0a557556f3d5b9e9441a447a9f6a1fa803c25032
Domenico <dom.dinicola@gmail.com>
2020-10-27 10:30:22 -0600

    py3: gettext_lazy
    ugettext_lazy is deprecated

--- src/crashlog/models.py
+++ src/crashlog/models.py
@@ -5,7 +5,7 @@ from django.db import models
 from django.db.models.query import QuerySet
 from django.utils.encoding import smart_str
 from django.utils.safestring import mark_safe
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class ErrorManager(models.Manager):
